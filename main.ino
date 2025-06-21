09836888dab372120fe46cfbd8d25de4366cf65d
Shonie Caplan <94767648+shoniecaplan@users.noreply.github.com>
2025-06-21 15:00:55 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -4,21 +4,20 @@
 // ###########################################
 
 // Constants #################################
-const int outputPin = A1; 
+const int SOIL_PIN = A1; 
 const int LED_PIN = 9;
 // ###########################################
 
 // Objects ###################################
 // Initialize the TCS34725 sensor
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
-
 // ###########################################
 
-int soilsensor(int analog_pin) {
+int read_soilsensor(int analog_pin) {
     return analogRead(analog_pin);
 }
 
-void startrgbsensor(){
+void start_rgbsensor() {
     if (tcs.begin()) {
         Serial.println("Found TCS34725 sensor");
         tcs.setInterrupt(true);  // Turn off LED until needed
@@ -28,31 +27,31 @@ void startrgbsensor(){
     }
 }
 
-void rgbcolorprint(r, g, b, c) {
+void print_rgbcolor(int r, int g, int b, int c) {
     // Normalize the color values
     float sum = (float)c;
     float red   = r / sum;
     float green = g / sum;
     float blue  = b / sum;
 
-    Serial.print("R: "); Serial.print(r);
-    Serial.print(" G: "); Serial.print(g);
-    Serial.print(" B: "); Serial.print(b);
-    Serial.print(" C: "); Serial.println(c);
+    // Serial.print("R: "); Serial.print(r);
+    // Serial.print(" G: "); Serial.print(g);
+    // Serial.print(" B: "); Serial.print(b);
+    // Serial.print(" C: "); Serial.println(c);
 
-    // Determine the dominant color
-    if (red > green && red > blue) {
-        Serial.println("Detected Color: RED");
-    } else if (green > red && green > blue) {
-        Serial.println("Detected Color: GREEN");
-    } else if (blue > red && blue > green) {
-        Serial.println("Detected Color: BLUE");
-    } else {
-        Serial.println("Detected Color: UNKNOWN");
-    }
+    // // Determine the dominant color
+    // if (red > green && red > blue) {
+    //     Serial.println("Detected Color: RED");
+    // } else if (green > red && green > blue) {
+    //     Serial.println("Detected Color: GREEN");
+    // } else if (blue > red && blue > green) {
+    //     Serial.println("Detected Color: BLUE");
+    // } else {
+    //     Serial.println("Detected Color: UNKNOWN");
+    // }
     uint16_t colorTemp = tcs.calculateColorTemperature(r, g, b);
     uint16_t lux       = tcs.calculateLux(r, g, b);
-    Serial.print("Color Temp: "); Serial.print(colorTemp); Serial.print(" K - ");
+    // Serial.print("Color Temp: "); Serial.print(colorTemp); Serial.print(" K - ");
     Serial.print("Lux: "); Serial.println(lux);
 }
 
@@ -65,25 +64,24 @@ void setup() {
     digitalWrite(LED_PIN, LOW);
 
     // Start RGB sensor
-    startrgbsensor();
+    start_rgbsensor();
 }
 
 void loop() {
+    Serial.println(read_soilsensor(SOIL_PIN));
+    
     // LED on RGB sensor HIGH
     digitalWrite(LED_PIN, HIGH);    // Turn on the LED for consistent lighting
-    delay(500);                     // Allow the LED to stabilize
+    delay(100);                     // Allow the LED to stabilize
 
     // Start printing color output/lux of RGB sensor
     uint16_t r, g, b, c;
     tcs.getRawData(&r, &g, &b, &c);
-    rgbcolorprint(r, g, b, c);
+    print_rgbcolor(r, g, b, c);
     
+
     digitalWrite(LED_PIN, LOW);     // Turn off the LED after reading
-    delay(2000);                    // Wait before the next reading
+    delay(500);                    // Wait before the next reading
 }
-
-
-
-
 
 
